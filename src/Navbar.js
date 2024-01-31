673eb5a5e73fc9642070cc617481b07b46aec2fb
Yahia Elkhashab <yahiaelkhashab17@gmail.com>
2024-01-31 10:44:36 +0200

    Adding Navbar

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+	return (
+		<nav className="navbar">
+			<h1>syntheia</h1>
+			<div className="links">
+				<a href="/">Home</a> |
+				<a href="/what we do">What we do</a> |
+				<a href="/products">Products</a> |
+				<a href="/create">New blog</a>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
