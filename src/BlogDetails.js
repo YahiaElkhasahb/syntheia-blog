d5605deff22ede7bc11acc4b5235bfdf977a50f6
YahiaElkhashab <yahiaelkhashab17@gmail.com>
2024-02-12 01:19:48 +0200

    Handle the process of deleting blogs and adding a 404 Not Found page

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,14 +1,23 @@
 import { useParams } from "react-router-dom";
 import LikesController from "./LikesController";
 import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 const BlogDetails = () => {
 	const { id } = useParams();
 	const { blogs, error, isPending } = useFetch(
 		"http://localhost:8000/blogs/" + id
 	);
+	const history = useHistory();
+	const handleClick = (e) => {
+		fetch("http://localhost:8000/blogs/" + blogs.id, {
+			method: "delete",
+		}).then(() => {
+			history.push("/");
+		});
+	};
 
 	return (
-		<div className="my-[10px] mx-[0px]">
+		<div className="my-[10px] mx-[0px] max-w-[700px]">
 			{isPending && <div>Loading...</div>}
 			{error && <div>{error}</div>}
 			{blogs && (
@@ -20,6 +29,12 @@ const BlogDetails = () => {
 					<LikesController />
 				</article>
 			)}
+			<button
+				className=" px-[5px] ml-[85%] hover:bg-[#e40f0fb9] rounded"
+				onClick={handleClick}
+			>
+				Delete
+			</button>
 		</div>
 	);
 };
---
+++ src/NotFound.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+const NotFound = () => {
+	return (
+		<div className="min-h-screen flex flex-grow justify-center">
+			<div className="rounded-lg bg-white p-8 text-center ">
+				<h1 className="mb-4 text-4xl font-bold">404</h1>
+				<p className="text-gray-600">
+					Oops! The page you are looking for could not be
+					found.
+				</p>
+				<Link
+					to="/"
+					className="mt-4 inline-block rounded bg-blue-500 px-4 py-2 font-semibold text-white hover:bg-blue-600"
+				>
+					{" "}
+					Go back to Home{" "}
+				</Link>
+			</div>
+		</div>
+	);
+};
+
+export default NotFound;
